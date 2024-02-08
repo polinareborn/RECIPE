5ef2abae09db9b00b3bdf87da58e16c126b3b796
Yura Test <yurec.ananikyan@mail.ru>
2024-02-08 21:11:42 +0300

    Task 1

---
+++ main.py
@@ -0,0 +1,12 @@
+def my_cook_book() :
+    with open('RECIPES.text', encoding='utf-8') as file:
+        cook_book = {}
+        for line in file.read().split('\n\n'):
+            name, _, *args = line.split('\n')
+            cook_li = []
+            for arg in args:
+                ingredient_name, quantity, measure = map(lambda x: int(x) if x.isdigit() else x, arg.split(' | '))
+                cook_li.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
+            cook_book[name] = cook_li
+    return cook_book
+
